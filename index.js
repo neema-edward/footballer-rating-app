c2e59dfaa036d07ac72990d601d2c377fcc1633f
Neema <neema.edward@student.moringaschool.com>
2025-03-26 13:07:29 +0300

    adds playerImage

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const baseURL = "http://localhost:3000/players";
 
-const playerName = document.getElementById("pname");
-const playerCurrentClub = document.getElementById("pcurrent-club");
-const playerPace = document.getElementById("ppace");
-const playerShooting = document.getElementById("pshooting");
-const playerPassing = document.getElementById("ppassing");
-const playerDribbling = document.getElementById("pdribbling");
-const playerDefence = document.getElementById("pdefence");
-const playerPhysical = document.getElementById("pphysical");
+const playerName = document.getElementById("p-name");
+const playerCurrentClub = document.getElementById("p-current-club");
+const playerPace = document.getElementById("p-pace");
+const playerShooting = document.getElementById("p-shooting");
+const playerPassing = document.getElementById("p-passing");
+const playerDribbling = document.getElementById("p-dribbling");
+const playerDefence = document.getElementById("p-defence");
+const playerPhysical = document.getElementById("p-physical");
+const playerRating = document.getElementById("p-rating");
+const playerImage = document.getElementById("im1")
+
+
+
