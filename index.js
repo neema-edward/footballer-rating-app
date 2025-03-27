a9bbc267026cb474ccf256321b382072f56b8fa2
Neema <neema.edward@student.moringaschool.com>
2025-03-27 09:23:26 +0300

    adds a variable to keep track of the players

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const baseURL = "http://localhost:3000/players";
 
 const playerName = document.getElementById("p-name");
-const playerCurrentClub = document.getElementById("p-current-club");
+const playerClub = document.getElementById("p-current-club");
 const playerPace = document.getElementById("p-pace");
 const playerShooting = document.getElementById("p-shooting");
 const playerPassing = document.getElementById("p-passing");
@@ -9,7 +9,8 @@ const playerDribbling = document.getElementById("p-dribbling");
 const playerDefence = document.getElementById("p-defence");
 const playerPhysical = document.getElementById("p-physical");
 const playerRating = document.getElementById("p-rating");
-const playerImage = document.getElementById("im1")
+const playerImage = document.getElementById("im1");
+const form = document.getElementById("rates-form");
 
-
-
+let players = [];
+let currentPlayer = 0;
